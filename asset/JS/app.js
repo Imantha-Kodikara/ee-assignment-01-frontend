0d67d72c04812c319474905e47d92a75c9e9e6e9
Imantha-Kodikara <imantha.l.kodikara@gmail.com>
2025-05-15 21:27:24 +0530

    JS added

--- asset/JS/app.js
+++ asset/JS/app.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+let studentName = document.getElementById("txt-name");
+let studentAddress = document.getElementById("txt-address");
+let studentAge = document.getElementById("txt-age");
+let btnGetDetails = document.getElementById("btn-get-details");
+
+function btnGetDetailsOnClick() {
+  let requestBody = {
+    name: studentName.value,
+    address: studentAddress.value,
+    age: studentAge.value,
+  };
+
+  fetch("http://localhost:8080/get-student-details", {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((data) => data.json)
+    .then((data) => {
+      console.log("Student Details Received....");
+    });
+}
+
+btnGetDetails.addEventListener("click", btnGetDetailsOnClick);
